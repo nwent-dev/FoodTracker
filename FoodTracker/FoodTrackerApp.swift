800b66354cf844c0c4030eda6740aaa8ae29ee7e
alk <nwent@proton.me>
2025-06-13 13:54:46 +0300

    Initial Commit

---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Александр Калашников on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
